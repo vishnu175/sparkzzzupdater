bbbed469bc82266f5ff6c81ab2c0308ff2cee0e4
VISHNU CS <mailvishnucs@gmail.com>
2020-10-02 18:27:25 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,40 +1,42 @@
-# credits @marshmello61
-# ported for SPARKZZZ
-repo="SPARKZZZ"
+# SPARKZZZ updater script 
+
+#get repo
+cd ..
+echo -n "Which repo you want to update? (SPARKZZZ) : "
+read repo
+echo " "
+
 rm -rf ${repo}
 
-echo "You're running the SPARKZZZ Updater script"
+echo "You're running the SPARKZZZ update script."
 echo " "
-# Get username of user
-echo -n "Enter your GitHub username: "
+
+#get username of user
+echo -n "Enter your github username: "
 read userName
 echo " "
 
-# Check if user has forked or has the same repo name
-echo -n "Do you forked from SPARKZZZ Repo or Do you have SPARKZZZ Repo? [y/n]: "
-read fork
-echo " "
+git clone https://github.com/${userName}/${repo}.git
 
-if [[ "${fork}" == 'y' ]]; then
-	git clone https://github.com/${userName}/${repo}.git
-	cd ${repo}
-elif [[ "${fork}" == 'n' ]]; then
-	echo -n "Enter your repo name: "
-	read repo
-	git clone https://github.com/${userName}/${repo}.git
-	cd ${repo}
+echo " "
+echo "Updating your ${repo} repo..."
+echo " "
+cd ${repo}
+
+if [[ "${repo}" = 'SPARKZZZ' ]]; then
+	git pull https://github.com/vishnu175/SPARKZZZ.git
+
 else
-        echo " "
-        echo "You were only supposed to enter y or n"
-        exit 1
+	echo -n "Enter the upstream repo's username: "
+	read ${upstream}
+	git pull https://github.com/${upstream}/${repo}.git
 fi
 
-echo " "
-echo "Updating your SPARKZZZ"
-git pull https://github.com/vishnu175/SPARKZZZ.git
-git commit -m "⚡"
+git add .
+git commit -m "Windows/termux merge"
 git push
 echo " "
 echo "Updated"
+echo " "
 cd ..
-rm -rf ${repo}
+cmd /k
